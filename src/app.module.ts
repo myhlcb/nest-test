fb3178399d41579e42825f2a0f724ef5467f3dc6
Simple <simple.ma@aax.com>
2022-11-16 14:22:26 +0800

    feat():add middleware and router

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,28 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DogService } from './dogs/dogs.service';
 import { DogsController } from './dogs/dogs.controller';
-
+import { LoggerMiddleware } from '../middleware/logger.middleware';
 @Module({
   imports: [],
   controllers: [AppController, DogsController],
   providers: [AppService, DogService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude(
+        { path: 'cats', method: RequestMethod.GET },
+        { path: 'cats', method: RequestMethod.POST },
+        'cats/(.*)',
+      )
+      .forRoutes({ path: 'common*', method: RequestMethod.ALL });
+  }
+}
